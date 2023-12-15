60724244dd2903520a39b6506e5e1281b241f496
totosuki <e231kari@gmail.com>
2023-12-15 14:21:25 +0900

    [glog.sh] 初期値を -5 に設定した

--- commands/glog.sh
+++ commands/glog.sh
@@ -3,5 +3,5 @@
 if [ -n "$1" ] ; then
   git log --graph -$1 --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
 else
-  git log --graph --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
+  git log --graph -5 --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
 fi
