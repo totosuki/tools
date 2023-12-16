9ba737c7bddc948b8c1ff80ed27207d47645aa14
totosuki <e231kari@gmail.com>
2023-12-16 19:29:54 +0900

    [glog.sh] setオプションでgit logの長さの初期値を変更できるように修正した

--- commands/glog.sh
+++ commands/glog.sh
@@ -1,7 +1,27 @@
 #!/bin/sh
 
+config_file_path="$HOME/tools/config/glog-default-length.txt"
+
+if [ ! -e "$config_file_path" ] ; then
+  echo 5 > "$config_file_path"
+fi
+
+val=$(<"$config_file_path")
+
 if [ -n "$1" ] ; then
-  git log --graph -$1 --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
+  case $1 in
+    -s | --set)
+      if [ -n "$2" ] ; then
+        echo $2 > $config_file_path
+        echo "git log で表示する文字の長さの初期値を${2}に変更しました"
+      else
+        echo "第二引数に数値が入力されていません。"
+      fi
+      ;;
+    *)
+      git log --graph -$1 --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
+      ;;
+  esac
 else
-  git log --graph -5 --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
+  git log --graph "-$val" --pretty="作成日: %Cblue %ci%Creset%Creset 作成者: %Cblue%an%Creset %Cred%d%Creset%n%Cgreen%t%Creset %m%m %s%n"
 fi
