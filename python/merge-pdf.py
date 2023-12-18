25ffc01f34d0a9ec7774f27704fecf35a9d9e70a
totosuki <e231kari@gmail.com>
2023-12-18 11:21:13 +0900

    [merge-pdf.py] pypdfがインストールされていない場合の処理を追加

--- python/merge-pdf.py
+++ python/merge-pdf.py
@@ -1,7 +1,11 @@
 # ２つの PDF を結合します
 # pip install pypdf してください
 
-from pypdf import PdfMerger
+try:
+  from pypdf import PdfMerger
+except:
+  print("'pypdf' がインストールされていません。'pip install pypdf' してください。")
+  exit(0)
 
 def merge_pdf(merger: PdfMerger, merge_cnt: int):
   for i in range(1, merge_cnt + 1):
