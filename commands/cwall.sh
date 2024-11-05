d4e9b88fd44a7de9e1f784a87d65a24037e33f35
totosuki <e231kari@gmail.com>
2024-11-05 13:26:21 +0900

    cwallにlogを出すようにした

--- commands/cwall.sh
+++ commands/cwall.sh
@@ -7,3 +7,6 @@ WALLPAPER=$(find $WALLPAPER_DIR -type f | shuf -n 1)
 
 # Change GNOME wallpaper
 gsettings set org.gnome.desktop.background picture-uri-dark "file://$WALLPAPER"
+
+# Change log wallpaper
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] change wallpaper: $WALLPAPER" >> $HOME/.cwall.log
