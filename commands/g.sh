770d82a90c1fd9b18bdf9018a2b42e40da869361
totosuki <e231kari@gmail.com>
2025-05-10 15:54:01 +0900

    g.shを修正

--- commands/g.sh
+++ commands/g.sh
@@ -1,49 +1,44 @@
 #!/bin/bash
-#
-# g [path …] <message>
-#   引数が 1 個   : message とみなす（add 対象は git add -A）
-#   引数が 2 個以上: 最後の引数を message、それ以外を add 対象 path とみなす
-#   commit 前に git status -s と commit message を表示して確認を求める
-#   push 後、glog が入っていれば最新 1 件を表示（※要 glog -1 対応版）
 
-set -euo pipefail
+function runcmd() 
+{
+    local pwd_out="$(pwd)"
+    echo -e "${pwd_out/#$HOME/~} $ $1"
+    eval "$1"
+}
 
 # prepare
 message="${@: -1}"
-if [ $# -eq 1 ]; then
-    git add -A
-    paths=()
-    else
-    paths=("${@:1:$# -1}")
-fi
 
-# log
-echo "Running git command..."
+# add & status
 if [ $# -eq 1 ]; then
-    echo "$ git add -A"
+    runcmd "git add -A"
 else
-    echo "$ git add $paths"
+    paths=("${@:1:$# -1}")
+    runcmd "git add ${paths}"
 fi
-echo "$ git status -s"
+runcmd "git status -s"
 
 # commit
-echo -en "Do you want to commit? (y/n) "
+echo -n "Do you want to commit? (y/n) "
 read -r answer
-if [ "$answer" = "y" ]; then
-    git commit -m "$message"
-else
+if [ "${answer}" != "y" ]; then
+    echo -n "Do you want to cancel staging? (y/n) "
+    read -r answer
+    if [ "${answer}" = "y" ]; then
+        runcmd "$ git restore --staged \$(git rev-parse --show-toplevel)"
+        echo "Staging cancelled."
+    fi
     echo "Commit cancelled."
     return 0
 fi
-
-# log
-echo -e "$ git commit -m \"$message\""
+runcmd "git commit -m \"${message}\""
 
 # push
-echo -en "\nDo you want to push? (y/n) "
+echo -n "Do you want to push? (y/n) "
 read -r answer
-if [ "$answer" = "y" ]; then
-    git push
+if [ "${answer}" = "y" ]; then
+    runcmd "git push"
 else
     echo "Push cancelled."
     return 0
